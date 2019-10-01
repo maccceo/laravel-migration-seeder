30239156beec4627ecfc33611cb7a2fe24d0be75
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-10-01 23:42:00 +0200

    Migration dei Places

---
+++ database/migrations/2019_10_01_213413_create_places_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlacesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('places', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->char('name', 100);
+            $table->string('address');
+            $table->char('city', 85);
+            $table->char('nation', 48);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('places');
+    }
+}
